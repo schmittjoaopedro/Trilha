ffbd070faaf1ee2dc8e5bd4a39d4589f6649dc0e
= <=>
2014-11-26 19:23:08 -0200

    adicionado tabela dados

--- src/main/webapp/home/gerenciar-eventos/js/app.js
+++ src/main/webapp/home/gerenciar-eventos/js/app.js
@@ -15,13 +15,17 @@ angular.module("App", ['mgcrea.ngStrap']).controller("EventosController", functi
         }
     });
     
-    $http.get("/Trilha/resources/evento", $scope.form).success(function(data){
-        angular.extend($scope, {
-            eventos: data
-        });
-    }).error(function(error){
-        alert("Ocorrou algum erro!");
-    });
+    function loadData() {
+	    $http.get("/Trilha/resources/evento", $scope.form).success(function(data){
+	        angular.extend($scope, {
+	            eventos: data
+	        });
+	    }).error(function(error){
+	        alert("Ocorrou algum erro!");
+	    });
+    }
+    
+    loadData();
     
     $scope.salvar = function(){
         $http.post("/Trilha/resources/evento", $scope.form).success(function(data){
@@ -36,6 +40,14 @@ angular.module("App", ['mgcrea.ngStrap']).controller("EventosController", functi
         $http.delete("/Trilha/resources/autenticacao").success(function(){
             window.location = "/Trilha";
         });
+    };
+    
+    $scope.remover = function(evento) {
+    	console.info(evento);
+    };
+    
+    $scope.modificar = function(evento) {
+    	$scope.form = angular.copy(evento);
     };
     
 });
